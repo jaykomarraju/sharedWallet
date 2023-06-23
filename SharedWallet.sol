5274b1a92da77dcd975f4b057755d53904ea363a
jaykomarraju <jay.komarraju@gmail.com>
2023-06-22 20:15:13 -0400

    Created 2 version
    - SharedWallet.sol: Factory Method
    - SharedWallets.sol: Single Contract Method

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+<!-- Factory Method: In the factory method version, we deploy a new contract instance for each shared wallet. This separates the state and functions for each wallet, which can provide isolation and security benefits but may be less efficient in terms of gas costs because of the need for repeated contract deployment. -->
+
 contract SharedWallet is AccessControl {
     uint256 constant votingThreshold = 51;
     uint256 constant proposalExpiryTime = 7 days;
@@ -10,12 +12,20 @@ contract SharedWallet is AccessControl {
 
     uint256 public ownerCount = 0;
 
+    enum ProposalType {
+        TRANSFER_FUNDS,
+        ADD_OWNER,
+        REMOVE_OWNER
+    }
+
     struct Proposal {
         string title;
         string description;
         uint256 amount;
         address payable recipient;
         uint256 deadline;
+        ProposalType proposalType;
+        bool isExecuted;
         mapping(address => bool) votes;
         uint256 totalVotes;
         bool isInitialized;
@@ -24,10 +34,12 @@ contract SharedWallet is AccessControl {
     mapping(uint256 => Proposal) public proposals;
     uint256 public proposalCounter;
     
-    event ProposalAdded(uint256 proposalId, string title, uint256 amount, address recipient);
+    event ProposalAdded(uint256 proposalId, string title, uint256 amount, address recipient, ProposalType proposalType);
     event VoteReceived(uint256 proposalId, address voter);
     event ProposalExecuted(uint256 proposalId, address executor);
     event OwnershipTransferred(address oldOwner, address newOwner);
+    event OwnerAdded(address newOwner);
+    event OwnerRemoved(address oldOwner);
 
     constructor(address[] memory _owners) {
         for (uint256 i = 0; i < _owners.length; i++) {
@@ -36,7 +48,7 @@ contract SharedWallet is AccessControl {
         }
     }
 
-    function createProposal(string memory _title, string memory _description, uint256 _amount, address payable _recipient) public onlyRole(OWNER_ROLE) {
+    function createProposal(string memory _title, string memory _description, uint256 _amount, address payable _recipient, ProposalType _proposalType) public onlyRole(OWNER_ROLE) {
         require(address(this).balance >= _amount, "Insufficient balance");
         
         Proposal storage newProposal = proposals[proposalCounter];
@@ -45,9 +57,11 @@ contract SharedWallet is AccessControl {
         newProposal.amount = _amount;
         newProposal.recipient = _recipient;
         newProposal.deadline = block.timestamp + proposalExpiryTime;
+        newProposal.proposalType = _proposalType;
+        newProposal.isExecuted = false;
         newProposal.isInitialized = true;
         
-        emit ProposalAdded(proposalCounter, _title, _amount, _recipient);
+        emit ProposalAdded(proposalCounter, _title, _amount, _recipient, _proposalType);
         proposalCounter++;
     }
 
@@ -62,22 +76,50 @@ contract SharedWallet is AccessControl {
 
     function executeProposal(uint256 _proposalId) public onlyRole(OWNER_ROLE) {
         require(proposals[_proposalId].isInitialized, "Proposal does not exist");
+        require(!proposals[_proposalId].isExecuted, "Proposal already executed");
         require(proposals[_proposalId].totalVotes * 100 / ownerCount >= votingThreshold, "Voting threshold not reached");
         require(proposals[_proposalId].deadline >= block.timestamp, "Proposal deadline has passed");
-        require(address(this).balance >= proposals[_proposalId].amount, "Insufficient balance");
-        proposals[_proposalId].recipient.transfer(proposals[_proposalId].amount);
+
+        if(proposals[_proposalId].proposalType == ProposalType.TRANSFER_FUNDS) {
+            require(address(this).balance >= proposals[_proposalId].amount, "Insufficient balance");
+            proposals[_proposalId].recipient.transfer(proposals[_proposalId].amount);
+        } else if(proposals[_proposalId].proposalType == ProposalType.ADD_OWNER) {
+            _setupRole(OWNER_ROLE, proposals[_proposalId].recipient);
+            ownerCount++;
+            emit OwnerAdded(proposals[_proposalId].recipient);
+        } else if(proposals[_proposalId].proposalType == ProposalType.REMOVE_OWNER) {
+            require(hasRole(OWNER_ROLE, proposals[_proposalId].recipient), "Address is not an owner");
+            revokeRole(OWNER_ROLE, proposals[_proposalId].recipient);
+            ownerCount--;
+            emit OwnerRemoved(proposals[_proposalId].recipient);
+        }
+
+        proposals[_proposalId].isExecuted = true;
         emit ProposalExecuted(_proposalId, msg.sender);
     }
 
-    function transferOwnership(address oldOwner, address newOwner) public onlyRole(OWNER_ROLE) {
-        require(hasRole(OWNER_ROLE, oldOwner), "Address is not an owner");
-        revokeRole(OWNER_ROLE, oldOwner);
-        grantRole(OWNER_ROLE, newOwner);
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-
-    // deposit to contract function: this function can be called by any owner to deposit ether to the contract
     function deposit() public payable {
         require(msg.value > 0, "Deposit amount must be greater than 0");
+    }
+}
+
+contract SharedWalletFactory {
+    mapping(address => address[]) public ownerWallets;
+    address[] public allWallets;
+
+    function createWallet(address[] memory _owners) public {
+        SharedWallet newWallet = new SharedWallet(_owners);
+        allWallets.push(address(newWallet));
+        for(uint i = 0; i < _owners.length; i++) {
+            ownerWallets[_owners[i]].push(address(newWallet));
+        }
+    }
+
+    function getWalletsByOwner(address owner) public view returns (address[] memory) {
+        return ownerWallets[owner];
+    }
+
+    function getAllWallets() public view returns (address[] memory) {
+        return allWallets;
     }
 }
---
+++ SharedWallets.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract SharedWallets is AccessControl {
+    uint256 constant votingThreshold = 51;
+    uint256 constant proposalExpiryTime = 7 days;
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+
+    struct Wallet {
+        mapping(address => bool) isOwner;
+        uint256 ownerCount;
+        uint256 balance;
+    }
+
+    struct Proposal {
+        string title;
+        string description;
+        uint256 amount;
+        address payable recipient;
+        uint256 deadline;
+        ProposalType proposalType;
+        bool isExecuted;
+        mapping(address => bool) votes;
+        uint256 totalVotes;
+        bool isInitialized;
+    }
+
+    enum ProposalType {
+        TRANSFER_FUNDS,
+        ADD_OWNER,
+        REMOVE_OWNER
+    }
+
+    mapping(address => Wallet) public wallets;
+    mapping(address => mapping(uint256 => Proposal)) public proposals;
+    mapping(address => uint256) public proposalCounters;
+
+    event ProposalAdded(address wallet, uint256 proposalId, string title, uint256 amount, address recipient, ProposalType proposalType);
+    event VoteReceived(address wallet, uint256 proposalId, address voter);
+    event ProposalExecuted(address wallet, uint256 proposalId, address executor);
+    event OwnershipTransferred(address wallet, address oldOwner, address newOwner);
+    event OwnerAdded(address wallet, address newOwner);
+    event OwnerRemoved(address wallet, address oldOwner);
+
+    function createWallet(address wallet, address[] memory owners) public {
+        require(wallets[wallet].ownerCount == 0, "Wallet already exists");
+        for (uint256 i = 0; i < owners.length; i++) {
+            wallets[wallet].isOwner[owners[i]] = true;
+            wallets[wallet].ownerCount++;
+        }
+    }
+
+    function deposit(address wallet) public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        require(wallets[wallet].isOwner[msg.sender], "Only owners can deposit");
+        wallets[wallet].balance += msg.value;
+    }
+
+    function createProposal(address wallet, string memory title, string memory description, uint256 amount, address payable recipient, ProposalType proposalType) public {
+        require(wallets[wallet].isOwner[msg.sender], "Only owners can create proposals");
+        require(wallets[wallet].balance >= amount, "Insufficient balance");
+
+        Proposal storage proposal = proposals[wallet][proposalCounters[wallet]];
+        proposal.title = title;
+        proposal.description = description;
+        proposal.amount = amount;
+        proposal.recipient = recipient;
+        proposal.deadline = block.timestamp + proposalExpiryTime;
+        proposal.proposalType = proposalType;
+        proposal.isExecuted = false;
+        proposal.isInitialized = true;
+
+        emit ProposalAdded(wallet, proposalCounters[wallet], title, amount, recipient, proposalType);
+        proposalCounters[wallet]++;
+    }
+
+    function voteProposal(address wallet, uint256 proposalId) public {
+        require(wallets[wallet].isOwner[msg.sender], "Only owners can vote");
+        Proposal storage proposal = proposals[wallet][proposalId];
+        require(proposal.isInitialized, "Proposal does not exist");
+        require(!proposal.votes[msg.sender], "You have already voted");
+        require(proposal.deadline >= block.timestamp, "Voting period has ended");
+
+        proposal.votes[msg.sender] = true;
+        proposal.totalVotes++;
+
+        emit VoteReceived(wallet, proposalId, msg.sender);
+    }
+
+    function executeProposal(address wallet, uint256 proposalId) public {
+        require(wallets[wallet].isOwner[msg.sender], "Only owners can execute proposals");
+        Proposal storage proposal = proposals[wallet][proposalId];
+        require(proposal.isInitialized, "Proposal does not exist");
+        require(!proposal.isExecuted, "Proposal already executed");
+        require(proposal.totalVotes * 100 / wallets[wallet].ownerCount >= votingThreshold, "Voting threshold not reached");
+        require(proposal.deadline >= block.timestamp, "Proposal deadline has passed");
+
+        if(proposal.proposalType == ProposalType.TRANSFER_FUNDS) {
+            require(wallets[wallet].balance >= proposal.amount, "Insufficient balance");
+            wallets[wallet].balance -= proposal.amount;
+            proposal.recipient.transfer(proposal.amount);
+        } else if(proposal.proposalType == ProposalType.ADD_OWNER) {
+            wallets[wallet].isOwner[proposal.recipient] = true;
+            wallets[wallet].ownerCount++;
+            emit OwnerAdded(wallet, proposal.recipient);
+        } else if(proposal.proposalType == ProposalType.REMOVE_OWNER) {
+            require(wallets[wallet].isOwner[proposal.recipient], "Address is not an owner");
+            wallets[wallet].isOwner[proposal.recipient] = false;
+            wallets[wallet].ownerCount--;
+            emit OwnerRemoved(wallet, proposal.recipient);
+        }
+
+        proposal.isExecuted = true;
+        emit ProposalExecuted(wallet, proposalId, msg.sender);
+    }
+}
